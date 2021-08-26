05f9729d8dfc432a4e5f1cfa2942228e4e888f1e
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-08-26 17:19:46 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 1 (Saída via Console) -  Faça um programa que exiba o nome da sua escola: Etec Adolpho Berezin.
+
+            Console.WriteLine("Etec Adolpho Berezin"); // Resolução da atividade.
+
+            Console.Write("Pressione qualquer botão para fechar. . . ");  // Indica a proxima ação que o usuário deve fazer.
+
+            Console.ReadKey(); //Vai aguardar uma ação do usuário para não encerrar automaticamente o programa.
+        }
+    }
+}
